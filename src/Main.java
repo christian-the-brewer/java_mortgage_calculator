443ba4828f5fb30c3dc3b65bcccc1b9318e3fcb9
christian-the-brewer <brewer.christiang@gmail.com>
2023-11-18 11:14:08 -0500

    finishes code without error handling

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    private static final int MONTHS_IN_A_YEAR = 12;
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // monthly payment = Principle * ( i ( 1 + i)^term ) / (( 1 + i )^term - 1 )
+        //create scanner
+        Scanner scanner = new Scanner(System.in);
+        //collect principal balance
+        System.out.println("Enter principal:");
+        double principal = scanner.nextDouble();
+        //collect annual interest rate
+        System.out.println("Enter annual interest rate:");
+        float annualInterestRate = scanner.nextFloat();
+        //collect years of loan term
+        System.out.println("Enter term for loan in years:");
+        int termInYears = scanner.nextInt();
+        //calculate monthly interest rate
+        float monthlyInterestRate = annualInterestRate / MONTHS_IN_A_YEAR;
+        //calculate number of monthly payments
+        int numberOfPayments = termInYears * MONTHS_IN_A_YEAR;
+        //calculate monthly payment
+        double monthlyPayment = principal * ((monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, numberOfPayments)))
+         / ((Math.pow(1 + monthlyInterestRate, numberOfPayments)) - 1 ));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        scanner.close();
+
+        System.out.println("Monthly payment: " + NumberFormat.getCurrencyInstance().format(monthlyPayment));
+        System.out.println("Total payback amount: " + NumberFormat.getCurrencyInstance().format(monthlyPayment * numberOfPayments));
     }
 }
